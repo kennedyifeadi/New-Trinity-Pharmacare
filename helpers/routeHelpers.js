9c4e231ac5108879de5ae0fad38f66e77fa79b3a
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-06-18 01:39:19 +0100

    Refactor isActiveRoute function to return 'page' instead of 'active' when the route matches the currentRoute

--- helpers/routeHelpers.js
+++ helpers/routeHelpers.js
@@ -1,5 +1,5 @@
 function isActiveRoute(route, currentRoute) {
-    return route === currentRoute ? 'active' : '';
+    return route === currentRoute && 'page';
   }
   
   module.exports = { isActiveRoute };
