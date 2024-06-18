a85c78d9fc83526f4be3ef7e5db5691590db6811
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-06-18 01:07:16 +0100

    Rendered contact Page

---
+++ handler.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+
+router.get("/", function (req, res) {
+    res.render("index", {
+      title: "Trinity Pharmacare-Home",
+      style: "index",
+      script: "index",
+      currentRoute: "/",
+      mainClass: "main_Content",
+    });
+  });
+
+  router.get("/contact", function (req, res) {
+    res.render("contact", {
+      title: "Contact",
+      style: "contact",
+      mainClass: "contact_mainContent",
+      currentRoute: "/contact",
+      script: "index"
+    });
+  });
+
+  router.get("/about", function (req, res) {
+    res.render("about", {
+      title: "Trinity Pharmacare-Home",
+      name: "about",
+      currentRoute: "/about",
+      script: false
+    });
+  });
+
+  router.get("/", function (req, res) {
+    res.render("home", {
+      title: "Trinity Pharmacare-Home",
+      name: "home",
+      currentRoute: "/",
+      script: true
+    });
+  });
+
+  router.get("/", function (req, res) {
+    res.render("home", {
+      title: "Trinity Pharmacare-Home",
+      name: "home",
+      currentRoute: "/",
+      script: true
+    });
+  });
+module.exports = router;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
 const { isActiveRoute } = require("./helpers/routeHelpers");
-
+const Handler = require("./handler");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
@@ -22,10 +22,7 @@ app.set("view engine", "ejs");
 
 app.locals.isActiveRoute = isActiveRoute;
 
-app.get("/", function (req, res) {
-  res.render("home");
-});
-
+app.use(Handler);
 app.all("*", function (req, res) {
     res.sendFile(path.join(__dirname, "error.html"));
   });
