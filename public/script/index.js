5f548ebc03973c5dbc1ff0d45353ade7bdf1239c
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-06-23 19:43:11 +0100

    Changed to 5000

--- public/script/index.js
+++ public/script/index.js
@@ -1,27 +1,13 @@
-const scrollers = document.querySelectorAll('.partnersScroller');
+const scrollers = document.querySelectorAll(".partnersScroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation()
+  addAnimation();
 }
 
-function addAnimation(){
-    scrollers.forEach(scroller =>{
-        scroller.setAttribute("data-animated", true)
-    });
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+  });
 }
-addAnimation()
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const carouselElement = document.getElementById('default-carousel');
-    const options = {
-        interval: 5000, // Set the interval to 5000 milliseconds (5 seconds)
-        indicators: {
-            activeClasses: 'bg-blue-500',
-            inactiveClasses: 'bg-gray-300'
-        },
-        pauseOnHover: true,
-        wrap: true
-    };
-    const carousel = new Carousel(carouselElement, options);
-});
+addAnimation();
+// if (Carousel)console.log(Carousel)
