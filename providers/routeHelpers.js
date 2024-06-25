2a32400c5dcd309f443aa03e1d1c7d8d120bf15f
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-06-25 08:35:16 +0100

    extended active class to mobile view

--- providers/routeHelpers.js
+++ providers/routeHelpers.js
@@ -1,5 +1,7 @@
 function isActiveRoute(route, currentRoute) {
-    return route === currentRoute ? 'md:text-blue-700 text-white' : "md:hover:text-blue-700 text-gray-700"
-  }
-  
-  module.exports = { isActiveRoute };
+  return route === currentRoute
+    ? "md:text-blue-700 text-white bg-blue-700 md:bg-transparent"
+    : "md:hover:text-blue-700 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent";
+}
+
+module.exports = { isActiveRoute };
