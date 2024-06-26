ced7c20e55ce47c5e74a8069495b0012304a6d2e
kennedyifeadi <kennedyifeadi@gmail.com>
2024-06-26 09:45:25 +0100

    updated styling in index.css and added full stop to services page

--- public/script/flowbite.js
+++ public/script/flowbite.js
@@ -1697,7 +1697,7 @@
                 inactiveClasses:
                   "bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
               },
-              interval: 5000,
+              interval: 8000,
               onNext: function () {},
               onPrev: function () {},
               onChange: function () {}
